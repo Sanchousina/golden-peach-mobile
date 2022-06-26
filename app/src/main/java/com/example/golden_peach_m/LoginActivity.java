c3e0076b60eefd3277d4eab361bc3bebcbc6edd4
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-06-26 18:36:21 +0200

    Created onClick activity

--- app/src/main/java/com/example/golden_peach_m/LoginActivity.java
+++ app/src/main/java/com/example/golden_peach_m/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.golden_peach_m;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_button = findViewById(R.id.button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AssetsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
