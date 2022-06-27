bb2242917de6ed763d93e562938e6399d95dfa24
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-06-27 20:25:01 +0200

    Add functionality to buttons on MetamaskNext

--- app/src/main/java/com/example/golden_peach_m/MetamaskNextActivity.java
+++ app/src/main/java/com/example/golden_peach_m/MetamaskNextActivity.java
@@ -2,13 +2,43 @@ package com.example.golden_peach_m;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MetamaskNextActivity extends AppCompatActivity {
 
+    TextView back;
+    Button cancel, connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metamask_next);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MetamaskActivity.class);
+                startActivity(intent);
+            }
+        });
+        cancel = findViewById(R.id.cancel_next);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        connect = findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AssetsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
