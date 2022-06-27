7478d5a63df22e9a7bcf4f5b8ede9b2fad8311ff
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-06-27 16:37:37 +0200

    Added onClick to Cancel button

--- app/src/main/java/com/example/golden_peach_m/MetamaskActivity.java
+++ app/src/main/java/com/example/golden_peach_m/MetamaskActivity.java
@@ -2,13 +2,26 @@ package com.example.golden_peach_m;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MetamaskActivity extends AppCompatActivity {
 
+    Button cancel_button, next_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metamask);
+        cancel_button = findViewById(R.id.cancel);
+        next_button = findViewById(R.id.next_button);
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
