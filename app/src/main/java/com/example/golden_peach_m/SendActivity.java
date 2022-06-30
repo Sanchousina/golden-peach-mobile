991960f589f614ef9920bd3fb46a24d1c9b4bd02
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-06-30 12:56:48 +0200

    Finished TransactionSentActivity

--- app/src/main/java/com/example/golden_peach_m/SendActivity.java
+++ app/src/main/java/com/example/golden_peach_m/SendActivity.java
@@ -2,13 +2,25 @@ package com.example.golden_peach_m;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SendActivity extends AppCompatActivity {
 
+    Button continue_send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+        continue_send = findViewById(R.id.continue_send);
+        continue_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SendActivity.this, TransactionSentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/golden_peach_m/TransactionSentActivity.java
@@ -0,0 +1,14 @@
+package com.example.golden_peach_m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionSentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_sent);
+    }
+}
