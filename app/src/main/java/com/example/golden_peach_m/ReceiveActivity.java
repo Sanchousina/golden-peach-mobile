8d85b0b885014f871f2e3ae42eac24bb2edbcc17
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-07-01 11:44:39 +0200

    Enabled copy to clipboard function

--- app/src/main/java/com/example/golden_peach_m/ReceiveActivity.java
+++ app/src/main/java/com/example/golden_peach_m/ReceiveActivity.java
@@ -2,18 +2,44 @@ package com.example.golden_peach_m;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ReceiveActivity extends AppCompatActivity {
 
-    ImageView back;
+    ImageView back, copy;
+    TextView address;
+
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
+
+        copy = findViewById(R.id.copy);
+        address = findViewById(R.id.address_to_copy);
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = address.getText().toString();
+                myClip = ClipData.newPlainText("wallet address", s);
+                myClipboard.setPrimaryClip(myClip);
+
+                Toast.makeText(getApplicationContext(), "Text Copied",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
         back = findViewById(R.id.logo_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +48,8 @@ public class ReceiveActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        copy = findViewById(R.id.copy);
+
     }
 }
